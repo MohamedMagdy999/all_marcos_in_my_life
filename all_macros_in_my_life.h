0e927a99fff520076c6847bcaca6b12a319112be
MohamedMagdy999 <amodi_99@yahoo.com>
2021-03-17 14:39:55 +0200

    edit and fix SwapEightBytes() macro

--- all_macros_in_my_life.h
+++ all_macros_in_my_life.h
@@ -5,9 +5,7 @@
 #define TOGGLE_BIT(REG, BIT) (REG ^= (1 << BIT))
 
 // Macro to Swap Nibbles in a Byte
-#define SwapNibbles(data) (((data & 0x0F) << 4) | ((data & 0xF0) >> 4))
-
-// Macro to Swap Nibbles in a Byte
+// SwapNibbles(0b 0011 0001 )  -> 0b 0001 0011
 #define SwapNibbles(data) (((data & 0x0F) << 4) | ((data & 0xF0) >> 4))
 
 // Macro to Swap Two Bytes
@@ -16,9 +14,10 @@
 // Macro to Swap two numbers
 #define SWAP(x, y) (x ^= y ^= x ^= y)
 
-#define ROR(REG, num) (REG = (REG >> num) | (REG << (8 - num)))
-
-#define ROL(REG, num) (REG = (REG << num) | (REG >> (8 - num)))
+// rotat bits from left and right
+// 0b 00110001 --> Rotate_from_right(,3)  --> 0b 00100110
+#define Rotate_from_right(REG, num) (REG = (REG >> num) | (REG << (8 - num)))
+#define Rotate_from_left(REG, num) (REG = (REG << num) | (REG >> (8 - num)))
 
 // check if the bit set or clear
 #define BIT_IS_SET(REG, BIT) (REG & (1 << BIT))
@@ -26,20 +25,11 @@
 
 // ---------------------------------
 
-#define constrain(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
-#define round(x) ((x) >= 0 ? (long)((x) + 0.5) : (long)((x)-0.5))
-
-#define interrupts() sei()
-#define noInterrupts() cli()
-
-#define clock_Cycles_Per_Microsecond() (F_CPU / 1000000L)
-#define clock_Cycles_To_Microseconds(a) ((a) / clockCyclesPerMicrosecond())
-#define microseconds_To_ClockCycles(a) ((a)*clockCyclesPerMicrosecond())
-
+// get the low and high 8-bits of the number
+// lowByte ( 0b 0011 0001 0000 0000 )  - > 0b 0000 0000  the low 8-bites
+// highByte ( 0b 0011 0001 0000 0000 )  - > 0b 0000 0000  the low 8-bites
 #define lowByte(w) ((uint8_t)((w)&0xff))
 #define highByte(w) ((uint8_t)((w) >> 8))
-
-// ------------------------------
 
 /* ———— Macros For Building BitFields ————– */
 
@@ -49,10 +39,6 @@
 #define BIT_CHECK(ADDRESS, BIT) (ADDRESS & (1 << BIT))
 #define BIT_FLIP(ADDRESS, BIT) (ADDRESS ^= (1 << BIT))
 #define BIT_GET(ADDRESS, BIT) (ADDRESS & (1 << BIT))
-
-/*___    common macros  _____ */
-
-/*_____mohamed yeheya macros _______*/
 
 /*_____from the ardino scr code */
 #define min(a, b) ((a) < (b) ? (a) : (b))
@@ -79,20 +65,23 @@
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
 
-
 // Macro to Convert one Endian into another Endian
-#define SwapEndians(value) ((value & 0x000000FF) << 24) | ((value &
-0x0000FF00) << 8) | \
-((value & 0x00FF0000) >> 8) | ((value & 0xFF000000) >> 24)
-  
-  
+#define SwapEndians(value) ((value & 0x000000FF) << 24) | ((value & 0x0000FF00) << 8) | \
+                               ((value & 0x00FF0000) >> 8) | ((value & 0xFF000000) >> 24)
+
 // Macro to Swap Eight Bytes of Data
-#define SwapEightBytes(data)
-((((data) >> 56) & 0x00000000000000FF) | (((data) >> 40) & 0x000000000000FF00) |
-(((data) >> 24) & 0x0000000000FF0000) | (((data) >> 8) & 0x00000000FF000000) |
-(((data) << 8) & 0x000000FF00000000) | (((data) << 24) & 0x0000FF0000000000) |
-(((data) << 40) & 0x00FF000000000000) | (((data) << 56) & 0xFF00000000000000))
-  
-// Macros to Get HIGH and LOW Bytes from a word
-#define LOW_BYTE(x) ((unsigned char)(x)&0xFF)
-#define HIGH_BYTE(x) ((unsigned char)(x>>8)&0xFF)
+#define SwapEightBytes(data) ((((data) >> 56) & 0x00000000000000FF) | (((data) >> 40) & 0x000000000000FF00) | \
+                              (((data) >> 24) & 0x0000000000FF0000) | (((data) >> 8) & 0x00000000FF000000) |  \
+                              (((data) << 8) & 0x000000FF00000000) | (((data) << 24) & 0x0000FF0000000000) |  \
+                              (((data) << 40) & 0x00FF000000000000) | (((data) << 56) & 0xFF00000000000000))
+
+// check if the amt value is between the two values
+#define constrain(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
+#define round(x) ((x) >= 0 ? (long)((x) + 0.5) : (long)((x)-0.5))
+
+#define interrupts() sei()
+#define noInterrupts() cli()
+
+#define clock_Cycles_Per_Microsecond() (F_CPU / 1000000L)
+#define clock_Cycles_To_Microseconds(a) ((a) / clockCyclesPerMicrosecond())
+#define microseconds_To_ClockCycles(a) ((a)*clockCyclesPerMicrosecond())
